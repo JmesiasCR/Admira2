8337827f632ad2b45b296aa9a6047833d3472dec
JmesiasCR <133353822+JmesiasCR@users.noreply.github.com>
2024-02-28 15:44:25 +0100

    Add files via upload

---
+++ videoPresentacion.js
@@ -0,0 +1,13 @@
+const myAtropos = Atropos({
+    el: '.my-atropos',
+    // rest of parameters
+    rotateLock: true,
+    rotateXMax: 15,
+    rotateYMax: 15,
+    shadow: true,
+    shadowOffset: 50,
+    shadowScale: 0.8,
+    highlight: false,
+    durationEnter: 600,
+    activeOffset: 60
+  });
