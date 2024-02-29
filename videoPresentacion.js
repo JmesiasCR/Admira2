5980d80728eafe712e5491970c7075e89ddf78d0
JmesiasCR <133353822+JmesiasCR@users.noreply.github.com>
2024-02-29 18:55:01 +0100

    Add files via upload

--- videoPresentacion.js
+++ videoPresentacion.js
@@ -5,9 +5,15 @@ const myAtropos = Atropos({
     rotateXMax: 15,
     rotateYMax: 15,
     shadow: true,
-    shadowOffset: 50,
+    shadowOffset: 30,
     shadowScale: 0.8,
     highlight: false,
     durationEnter: 600,
     activeOffset: 60
   });
+  
+  function handleButtonClick() {
+    // Aquí puedes colocar el código que se ejecutará cuando se haga clic en el botón.
+    // Puedes redirigir a PagPrinicipal.html usando window.location.href
+    window.location.href = 'PagPrinicipal.html';
+}
