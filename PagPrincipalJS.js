44bb7c55f8eaa7f1a696aa0c5b4e31d605456450
JmesiasCR <133353822+JmesiasCR@users.noreply.github.com>
2024-03-07 19:03:37 +0100

    Add files via upload
    07_03_2024

--- PagPrincipalJS.js
+++ PagPrincipalJS.js
@@ -1,7 +1,10 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.opacity = "1"; // Asegura que el sidebar sea visible al abrir
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    setTimeout(function() { document.getElementById("mySidenav").style.opacity = "0"; }, 500); // Espera a que se complete la transición de ancho antes de ocultar
 }
+
